0750a8b32540a3ee9257b50f45d1f9500da1f84f
Anders Kaare <anders@straadt.dk>
2018-08-21 00:48:30 +0200

    mfd button holes

--- example_mfd.cc
+++ example_mfd.cc
@@ -8,6 +8,9 @@ const double corner_radius = 0.5;
 const double screen_r1 = 0.5;
 const double screen_r2 = 0.3;
 const double screen_depth = 0.5;
+const int n_buttons_per_side = 5;
+const double button_spacing = 1.4;
+const double button_size = 0.9;
 
 void cgmain()
 {
@@ -24,10 +27,44 @@ void cgmain()
 			);
 		};
 
+		auto button_holes = []{
+			auto button_hole = [](double sx, double sy) {
+				translate(-sx/2, -sy/2, -1) box(sx, sy, depth+2);
+			};
+
+			for (int i = 0; i < n_buttons_per_side; i++) {
+				double d = side/2 + (i - (double)n_buttons_per_side/2 + 0.5) * button_spacing;
+				auto std_button_hole = [&]() {
+					button_hole(button_size, button_size);
+				};
+				translate(d,margin/2) std_button_hole();
+				translate(d,side-margin/2) std_button_hole();
+				translate(margin/2,d) std_button_hole();
+				translate(side-margin/2,d) std_button_hole();
+			}
+
+			auto xx_button = [&](double x0, double x1) {
+				double sx = x1-x0;
+				double dx = x0+sx/2;
+				translate(dx,margin/2) button_hole(sx, button_size);
+			};
+
+			xx_button(
+				margin + screen_r2,
+				side/2 - button_spacing * ((double)n_buttons_per_side/2) - (button_spacing - button_size)/2
+			);
+
+			xx_button(
+				side/2 + button_spacing * ((double)n_buttons_per_side/2) + (button_spacing - button_size)/2,
+				side - margin - screen_r2
+			);
+		};
+
 		translate(-side/2, -side/2) {
 			cut {
 				panel_outline();
 				screen_cut();
+				button_holes();
 			}
 		}
 	}
