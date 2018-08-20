565fff5436ff968ad081297404cc12e1e09321a6
Anders Kaare <anders@straadt.dk>
2018-08-20 21:13:32 +0200

    boolean op stress testing
    takes some time to compile, but the mesh is beautiful :'-) OpenCASCADE appears
    to be quite good at triangulation

--- example_boolean_ops.cc
+++ example_boolean_ops.cc
@@ -13,8 +13,27 @@ void cgmain()
 			rotate(90_Y) cyl();
 		};
 
+		/* simple examples; cut/fuse/common (aka difference/union/intersection) */
 		translate(-4) cut    objs();
 		              fuse   objs();
 		translate(4)  common objs();
+
+		/* do some stress testing; perforate a box with a bunch of cylinders */
+		translate(0,10) translate(-5,-5) {
+			cut {
+				translate(-0.5_Z) box(10,10,1);
+				for (int x = 0; x <= 10; x++) {
+					for (int y = 0; y <= 10; y++) {
+						translate(x, y, -1) cylinder(0.3, 4);
+					}
+				}
+				for (int x = 0; x <= 10; x++) {
+					translate(x) rotate(-90_X) translate(-1_Z) cylinder(0.3, 12);
+				}
+				for (int y = 0; y <= 10; y++) {
+					translate(0,y) rotate(90_Y) translate(-1_Z) cylinder(0.3, 12);
+				}
+			}
+		}
 	}
 }
