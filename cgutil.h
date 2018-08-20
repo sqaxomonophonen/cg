f1f67581c61273d68c5212d962703e0d2954bb44
Anders Kaare <anders@straadt.dk>
2018-08-21 00:33:51 +0200

    reusable component

--- cgutil.h
+++ cgutil.h
@@ -20,5 +20,33 @@ void z_rounded_box(double sx, double sy, double sz, double r)
 	prism(v3(0,0,sz)) z_rounded_quad(sx, sy, r);
 }
 
+void cone_corner_cutter(double sx, double sy, double z0, double z1, double z2, double z3, double r0, double r1)
+{
+	fuse {
+		const double dr = r1-r0;
+		const double dr2 = dr*2;
+		const double wd = z2-z1;
+		translate(0,0,z0) translate(dr, dr) z_rounded_box(sx-dr2, sy-dr2, z2-z0, r0);
+		translate(0,0,z1) {
+			auto mk_cone = [&](){ cone(r0,r1,wd); };
+			translate(r1, r1) mk_cone();
+			translate(sx-r1,r1) mk_cone();
+			translate(r1,sy-r1) mk_cone();
+			translate(sx-r1,sy-r1) mk_cone();
+		}
+
+		const double wh = 3;
+		const double wlx = sx - r1*2;
+		const double wly = sy - r1*2;
+		translate(0,0,z1 + wh) {
+			auto mk_wedge = [&](const double wl) { wedge(wh,r1-r0,wl,wh-wd); };
+			translate(r1, sy-dr) rotate(90_Y) mk_wedge(wlx);
+			translate(sx-r1, dr) rotate(180_Z) rotate(90_Y) mk_wedge(wlx);
+			translate(dr, r1) rotate(90_Z) rotate(90_Y) mk_wedge(wly);
+			translate(sx-dr,sy-r1) rotate(-90_Z) rotate(90_Y) mk_wedge(wly);
+		}
+	}
+}
+
 #define CGUTIL_H
 #endif
--- example_mfd.cc
+++ example_mfd.cc
@@ -1,13 +1,13 @@
 #include "cgmain.h"
 #include "cgutil.h"
 
-double side = 15;
-double depth = 1;
-double margin = 1.5;
-double corner_radius = 0.5;
-double screen_r1 = 0.5;
-double screen_r2 = 0.3;
-double screen_depth = 0.5;
+const double side = 15;
+const double depth = 1;
+const double margin = 1.5;
+const double corner_radius = 0.5;
+const double screen_r1 = 0.5;
+const double screen_r2 = 0.3;
+const double screen_depth = 0.5;
 
 void cgmain()
 {
@@ -17,22 +17,11 @@ void cgmain()
 		};
 
 		auto screen_cut = []{
-			double smm = side - margin*2;
-			translate(margin,margin,-depth) z_rounded_box(smm, smm, depth*3, screen_r2);
-			translate(0,0,depth-screen_depth) {
-				translate(margin+screen_r2, margin+screen_r2) cone(screen_r2, screen_r1, screen_depth);
-				translate(side-margin-screen_r2, margin+screen_r2) cone(screen_r2, screen_r1, screen_depth);
-				translate(margin+screen_r2, side-margin-screen_r2) cone(screen_r2, screen_r1, screen_depth);
-				translate(side-margin-screen_r2, side-margin-screen_r2) cone(screen_r2, screen_r1, screen_depth);
-			}
-			double wh = 3;
-			double wl = side - margin*2 - screen_r2*2;
-			translate(0,0,wh+depth-screen_depth) {
-				translate(margin + screen_r2, side - margin) rotate(90_Y) wedge(wh,screen_r1-screen_r2,wl,wh - screen_depth);
-				translate(side - margin - screen_r2, margin) rotate(180_Z) rotate(90_Y) wedge(wh,screen_r1-screen_r2,wl,wh - screen_depth);
-				translate(margin, margin + screen_r2) rotate(90_Z) rotate(90_Y) wedge(wh,screen_r1-screen_r2,wl,wh - screen_depth);
-				translate(side - margin, side - margin - screen_r2) rotate(-90_Z) rotate(90_Y) wedge(wh,screen_r1-screen_r2,wl,wh - screen_depth);
-			}
+			translate(margin,margin) cone_corner_cutter(
+				side-margin*2, side-margin*2,
+				-1, depth-screen_depth, depth, depth+1,
+				screen_r2, screen_r1
+			);
 		};
 
 		translate(-side/2, -side/2) {
@@ -41,6 +30,5 @@ void cgmain()
 				screen_cut();
 			}
 		}
-
 	}
 }
